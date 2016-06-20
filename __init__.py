7e729e82679210ab5937a78b3f0c643e175b4032
Lorenz Drescher <dev@tmtnslt.com>
2016-06-20 22:27:55 +0200

    Added primitive to translate numpy array to t_chitframe

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ import ctypes
 import threading
 import queue
 import time
+import numpy as np
 
 q = queue.Queue()
 light = Light(5,8)
@@ -30,12 +31,29 @@ def init_shitlight():
     st.start()
 
 def add_frame(rep, frame):
-    # adapt chitframe to simulator color
-    color = []
-    for br in frame.brightness:
-        for led in br:
-            color.append((led[0],led[1],led[2]))
-    q.put((rep, color))
+    if type(frame) is t_chitframe:
+        # adapt chitframe to simulator color
+        color = []
+
+        for br in frame.brightness:
+            for led in br:
+                color.append((led[0],led[1],led[2]))
+        q.put((rep, color))
+    elif type(frame) is np.ndarray:
+        # adapt numpy to t_chitframe. Backport to real shitlight!
+        temp_frame = t_chitframe()
+        # TODO: Trunkate array if longer than our t_chitframe dimensions.
+        # TODO: Scale to ubyte if given as float or double
+        temp_frame.brightness = np.ctypeslib.as_ctypes(frame.astype("ubyte"))
+        
+        color = [] 
+        for br in temp_frame.brightness:
+            for led in br:
+                color.append((led[0],led[1],led[2]))
+        q.put((rep, color))
+
+    else:
+        raise NotImplementedError
 
 def get_fps():
     return 100
